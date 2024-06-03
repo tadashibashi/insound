321f697da92b89ba2edb17240bc9ecc83040bba6
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-06-03 07:25:52 -0700

    update error api: add system errors, add new error types

--- insound/Error.cpp
+++ insound/Error.cpp
@@ -4,30 +4,100 @@
 #include <utility>
 
 namespace insound {
-    static std::stack<Error> s_errors;
+    /// Each thread has its own error stack to prevent data races
+    thread_local static std::stack<Result> s_errors;    // client error stack
+    thread_local static std::stack<Result> s_sysErrors; // used by the program
+    static const Result NoErrors {Result::Ok, nullptr};
 
-    Error::Error(Code code, std::string message): code(code), message(std::move(message))
+    static const char *s_codeNames[] = {
+        "No errors.",
+        "SDL Error",
+        "Runtime Error",
+        "Logic Error",
+        "Invalid Argument",
+        "Invalid Handle",
+    };
+
+    Result::Result(Code code, const char *message): code(code), message(message)
     { }
 
-    void pushError(Error::Code code, const std::string &message)
+    void pushError(Result::Code code, const char *message)
     {
 #if INSOUND_DEBUG || INSOUND_LOG_ERRORS
-        fprintf(stderr, "MACH ERROR: %s\n", s_errors.emplace(code, message).message.c_str());
+        if (message)
+        {
+            fprintf(stderr, "INSOUND ERROR: %s: %s\n", s_codeNames[code], s_errors.emplace(code, message).message);
+        }
+        else
+        {
+            fprintf(stderr, "INSOUND ERROR: %s\n", s_codeNames[code]);
+            s_errors.emplace(code, "");
+        }
 #else
-        s_errors.emplace(code, message);
+        s_errors.emplace(code, message ? message : "");
 #endif
     }
 
-    Error getError()
+    Result popError()
     {
         if (s_errors.empty())
         {
-            return {Error::Ok, "No errors."};
+            return NoErrors;
         }
 
-        auto err = s_errors.top();
+        const auto err = s_errors.top();
         s_errors.pop();
 
         return err;
+    }
+
+    const Result &peekError()
+    {
+        return (s_errors.empty()) ? NoErrors : s_errors.top();
+    }
+
+    bool lastErrorIs(Result::Code code)
+    {
+        return peekError().code == code;
+    }
+
+    Result detail::popSystemError()
+    {
+        if (s_sysErrors.empty())
+        {
+            return NoErrors;
+        }
+
+        const auto &err = s_sysErrors.top();
+        s_sysErrors.pop();
+
+        return err;
+    }
+
+    const Result &detail::peekSystemError()
+    {
+        return s_errors.empty() ? NoErrors : s_sysErrors.top();
+    }
+
+    void detail::pushSystemError(Result::Code code, const char *message)
+    {
+#if INSOUND_DEBUG || INSOUND_LOG_ERRORS
+        if (message)
+        {
+            fprintf(stderr, "INSOUND ERROR: %s: %s\n", s_codeNames[code], s_errors.emplace(code, message).message);
+        }
+        else
+        {
+            fprintf(stderr, "INSOUND ERROR: %s\n", s_codeNames[code]);
+            s_errors.emplace(code, "");
+        }
+#else
+        s_errors.emplace(code, message ? message : "");
+#endif
+    }
+
+    bool hasError()
+    {
+        return !s_errors.empty();
     }
 }
--- insound/Error.h
+++ insound/Error.h
@@ -3,21 +3,40 @@
 #include <string>
 
 namespace insound {
-    struct Error {
+    struct Result {
         enum Code {
-            Ok,
-            SdlErr,
-            RuntimeErr,
-            LogicErr,
-            InvalidArg,
+            Ok,            ///< No errors.
+            SdlErr,        ///< SDL runtime error.
+            RuntimeErr,    ///< Insound runtime error.
+            LogicErr,      ///< Most likely a user logic error.
+            InvalidArg,    ///< Invalid argument passed to a function.
+            InvalidHandle, ///< Attempted to use invalid handle.
+            EngineNotInit,
         };
 
-        Error(Code code, std::string message);
+        Result(Code code, const char *message);
 
         Code code;
-        std::string message;
+        const char *message;
     };
 
-    void pushError(Error::Code code, const std::string &message = "");
-    Error getError();
+    void pushError(Result::Code code, const char *message = nullptr);
+
+    [[nodiscard]]
+    Result popError();
+
+    [[nodiscard]]
+    bool hasError();
+
+    [[nodiscard]]
+    const Result &peekError();
+
+    bool lastErrorIs(Result::Code code);
+
+    namespace detail {
+        void pushSystemError(Result::Code code, const char *message = nullptr);
+        Result popSystemError();
+        const Result &peekSystemError();
+    }
+
 }
