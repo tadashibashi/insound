d96e46c7d35666c3d608d43362aa7490fe1b651b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-06-03 07:24:22 -0700

    Effect: replace sendParam/receiveParam with sendInt, sendFloat, sendString, receiveInt, receiveFloat, receiveString

--- insound/Effect.cpp
+++ insound/Effect.cpp
@@ -1,9 +1,47 @@
 #include "Effect.h"
 #include "Engine.h"
 #include "Command.h"
+
 namespace insound {
-    void Effect::sendParam(int index, float value)
+
+    void Effect::sendFloat(int index, float value)
     {
-        m_engine->pushCommand(Command::makeEffectParamSet(this, index, value));
+        m_engine->pushCommand(Command::makeEffectSetFloat(this, index, value));
     }
-} // insound
+
+    void Effect::sendInt(int index, int value)
+    {
+        m_engine->pushCommand(Command::makeEffectSetInt(this, index, value));
+    }
+
+    void Effect::sendString(int index, const char *value)
+    {
+        m_engine->pushCommand(Command::makeEffectSetString(this, index, value));
+    }
+
+    void Effect::applyCommand(const EffectCommand &command)
+    {
+        switch(command.type)
+        {
+            case EffectCommand::SetFloat:
+            {
+                receiveFloat(command.setfloat.index, command.setfloat.value);
+            } break;
+
+            case EffectCommand::SetInt:
+            {
+                receiveInt(command.setint.index, command.setint.value);
+            } break;
+
+            case EffectCommand::SetString:
+            {
+                receiveString(command.setstring.index, command.setstring.value);
+            } break;
+
+            default:
+            {
+
+            } break;
+        }
+    }
+}
--- insound/Effect.h
+++ insound/Effect.h
@@ -1,29 +1,56 @@
 #pragma once
 
 namespace insound {
+    struct EffectCommand;
     class Engine;
 
     class Effect {
     public:
-        explicit Effect(Engine *engine) : m_engine(engine) { }
         virtual ~Effect() = default;
-
-        virtual void process(float *input, float *output, int count) = 0;
+    protected:
+        Effect() : m_engine() { }
+        void sendFloat(int index, float value);
+        void sendInt(int index, int value);
+        void sendString(int index, const char *value);
 
         [[nodiscard]]
         Engine *engine() { return m_engine; }
         [[nodiscard]]
         const Engine *engine() const { return m_engine; }
-    protected:
-        void sendParam(int index, float value);
-
-        /// Override this if you need to process parameter sets.
-        /// Don't set member parameters directly as it may occur during audio thread.
-        /// Instead set via `setParam` and apply changes in `receiveParam`.
-        virtual void receiveParam(int index, float value) {}
-
     private:
         friend class Engine;
+        friend class SoundSource;
+        void applyCommand(const EffectCommand &command);
+
+        /// Override this if you need to process float parameter sets.
+        /// Don't set member parameters directly as it may occur during audio thread,
+        /// set via `send*` functions and apply changes in `receive*` functions.
+        /// @param index index passed in `sendFloat` now received
+        /// @param value value passed in `sendFloat` now received
+        virtual void receiveFloat(int index, float value) {}
+
+        /// Override this if you need to process integer parameter sets.
+        /// Don't set member parameters directly as it may occur during audio thread,
+        /// set via `sendInt` functions and apply changes in this function.
+        /// @param index index passed in `sendInt` now received
+        /// @param value value passed in `sendInt` now received
+        virtual void receiveInt(int index, int value) {}
+
+        /// Override this if you need to process string parameter sets.
+        /// Don't set member parameters directly as it may occur during audio thread,
+        /// set via `sendString` functions and apply changes in `receiveString` functions.
+        /// @param index index passed in `sendString` now received
+        /// @param value value passed in `sendString` now received
+        virtual void receiveString(int index, const char *value) {}
+
+        /// Required to override for this effect's processing logic
+        /// @param input  input buffer filled with data to process
+        /// @param output output buffer to write to (comes cleared to 0)
+        /// @param count  number of samples, the length of both input and output arrays. This value is guaranteed to
+        ///               be a multiple of 4 for optimization purposes.
+        /// @note both input and output buffers are interleaved stereo, so samples will occur in L-R-L-R order
+        virtual void process(float *input, float *output, int count) = 0;
+
         Engine *m_engine;
     };
 }
