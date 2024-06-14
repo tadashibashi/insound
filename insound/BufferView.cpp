6038a5df8a2b4549b773e9c0620e7e4fd1429671
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-06-13 20:33:30 -0700

    add BufferView and BufferWriter classes from sdgl repo, adapt for this project

---
+++ insound/BufferView.cpp
@@ -0,0 +1,200 @@
+#include "BufferView.h"
+#include "Error.h"
+
+#include <cassert>
+
+namespace insound {
+    uint32_t BufferView::read(std::string &outString, const size_t maxSize)
+    {
+        // Current position already finished reading?
+        if (m_pos >= m_size)
+        {
+            pushError(Result::EndOfBuffer, "Cannot read string from buffer because BufferView is done reading");
+            return 0;
+        }
+
+        try
+        {
+            // find the null terminator's position (end of the string)
+            size_t termPos = m_pos;
+            while (termPos < m_size && termPos - m_pos < maxSize && m_buf[termPos] != 0)
+            {
+                ++termPos;
+            }
+
+            // copy the data
+            outString = std::string(m_buf + m_pos, m_buf + termPos);
+
+            if (termPos < m_size && m_buf[termPos] != 0) // last position not a terminator from early clipping from maxSize?
+            {
+                // finish until end of the string
+                while(termPos < m_size && m_buf[termPos] != 0)
+                    ++termPos;
+            }
+
+            m_pos = termPos + 1;
+            return outString.size() + 1u; // +1 includes the null-terminator
+        }
+        catch(const std::exception &e)
+        {
+            pushError(Result::StdExcept, e.what());
+            return 0;
+        }
+        catch(...)
+        {
+            pushError(Result::UnknownErr, "Failed to read string from buffer: unknown error");
+            throw;
+        }
+    }
+
+    uint32_t BufferView::readFixedString(std::string &outString, size_t length)
+    {
+        if (m_pos + length > m_size)
+        {
+            pushError(Result::EndOfBuffer, "Size of read exceeds buffer size");
+            return 0;
+        }
+
+        outString = std::string(m_buf + m_pos, m_buf + m_pos + length);
+        m_pos += length + 1; // +1 to move one past null terminator
+        return length + 1u;
+    }
+
+    uint32_t BufferView::read(char *outBuffer, const size_t maxSize)
+    {
+        // Current position already finished reading?
+        if (m_pos >= m_size)
+        {
+            pushError(Result::EndOfBuffer, "Cannot read string from buffer because BufferView is done reading");
+            return 0;
+        }
+
+        try
+        {
+            // find the null terminator's position
+            size_t termPos = m_pos;
+            while (termPos < m_size &&
+                termPos - m_pos < maxSize - 1 &&  // must be `maxSize-1` to leave room for null terminator
+                m_buf[termPos] != 0)
+            {
+                ++termPos;
+            }
+
+            std::memcpy(outBuffer, m_buf + m_pos, termPos - m_pos);
+            outBuffer[termPos - m_pos] = 0; // set terminator in the output string
+
+            if (termPos < m_size && m_buf[termPos] != 0) // last index not a null terminator due to early break from clipping?
+            {
+                while (termPos < m_size && m_buf[termPos] != 0) // finish until end of string
+                    ++termPos;
+            }
+
+            auto length = termPos - m_pos + 1; // + 1 includes the null terminator
+            m_pos = termPos + 1;
+            return length;
+        }
+        catch(const std::exception &e)
+        {
+            pushError(Result::StdExcept, e.what());
+            return 0;
+        }
+        catch(...)
+        {
+            pushError(Result::UnknownErr, "Failed to read string from buffer: unknown error");
+            throw;
+        }
+    }
+
+
+
+    uint8_t BufferView::peek(const int offset) const
+    {
+        const int index = static_cast<int>(m_pos) + offset;
+
+        assert(index >= 0 && index < m_size);
+
+        return m_buf[index];
+    }
+
+    uint32_t BufferView::readImpl(void *buffer, const size_t size)
+    {
+        if (size == 0) return 0; // edge case
+
+        if (m_pos + size > m_size)
+        {
+            pushError(Result::EndOfBuffer, "Size of read exceeds buffer size");
+            return 0;
+        }
+
+        if (size > 1 && endian::native != m_endian) // endianness is opposite
+        {
+            // Copy bytes in reverse order
+            auto dest = (uint8_t *)buffer;
+            auto source = (uint8_t *)(m_buf + m_pos + size - 1);
+            while (source >= m_buf + m_pos)
+            {
+                *dest++ = *source--;
+            }
+        }
+        else
+        {
+            // Copy bytes in normal order
+            std::memcpy(buffer, m_buf + m_pos, size);
+        }
+
+        m_pos += size;
+        return size;
+    }
+
+    uint32_t BufferWriter::writeImpl(const void *data, size_t size)
+    {
+        if (size == 0) return 0; // edge case
+
+        if (m_pos + size > m_size)
+        {
+            pushError(Result::EndOfBuffer, "Size of read exceeds buffer size");
+            return 0;
+        }
+
+        if (size > 1 && endian::native != m_endian)
+        {
+            // Write bytes in reverse order
+            auto dest = m_buf + m_pos;
+            auto source = (const uint8_t *)data + size - 1;
+            while (source >= data)
+            {
+                *dest++ = *source--;
+            }
+        }
+        else
+        {
+            std::memcpy(m_buf + m_pos, data, size);
+        }
+
+        m_pos += size;
+        return size;
+    }
+
+    uint32_t BufferWriter::write(const std::string &str)
+    {
+        return write(str.data(), str.size());
+    }
+
+    uint32_t BufferWriter::write(const std::string_view str)
+    {
+        return write(str.data(), str.size());
+    }
+
+    uint32_t BufferWriter::write(const char *str, size_t length)
+    {
+        if (length + m_pos + 1 > m_size) // +1 for null terminator
+        {
+            pushError(Result::EndOfBuffer, "Size of read exceeds buffer size");
+            return 0;
+        }
+
+        std::memcpy(m_buf + m_pos, str, length);
+        m_buf[m_pos + length] = '\0';
+        return length + 1;
+    }
+}
---
+++ insound/BufferView.h
@@ -0,0 +1,176 @@
+#pragma once
+
+#include "util.h"
+
+#include <string>
+#include <vector>
+
+namespace insound {
+
+    /// Helper to view a memory buffer with both read and write functionality
+    /// @warning this should only be used on known data types
+    class BufferView
+    {
+    public:
+        /// Create BufferView from a buffer
+        /// @param buffer pointer to memory to view
+        /// @param size size of the buffer in bytes
+        /// @param endianness expected endianness of numeric data types (strings are always handled in little-endian order)
+        BufferView(const void *buffer, const size_t size, const endian::type endianness = endian::little) :
+            m_buf((uint8_t *)buffer), m_pos(0), m_size(size), m_endian(endianness)
+        {}
+
+        /// Create BufferView from null-terminated c-string
+        /// @param buffer c string with memory to view
+        /// @param endianness expected endianness of numeric data types (strings are always handled in little-endian order)
+        explicit BufferView(const char *buffer, const endian::type endianness = endian::little) :
+            m_buf((uint8_t *)buffer), m_pos(0), m_size(std::strlen(buffer)), m_endian(endianness)
+        {}
+
+        /// Create BufferView from string of byte data
+        /// @param buffer string containing memory to view
+        /// @param endianness expected endianness of numeric data types (string are always handled in little-endian order)
+        explicit BufferView(const std::string &buffer, const endian::type endianness = endian::little) :
+            m_buf((uint8_t *)buffer.data()), m_pos(0), m_size(buffer.length()), m_endian(endianness)
+        {}
+
+        /// Create BufferView from vector of uint8_t
+        /// @param buffer vector containing memory to view
+        /// @param endianness expected endianness of numeric data types (strings are always handled in little-endian order)
+        explicit BufferView(const std::vector<uint8_t> &buffer, const endian::type endianness = endian::little) :
+            m_buf((uint8_t *)buffer.data()), m_pos(0), m_size(buffer.size()), m_endian(endianness)
+        {}
+
+        /// Create BufferView from vector of bytes
+        /// @param buffer vector containing memory to view
+        /// @param endianness expected endianness of numeric data types (strings are always handled in little-endian order)
+        explicit BufferView(const std::vector<char> &buffer, const endian::type endianness = endian::little) :
+            m_buf((uint8_t *)buffer.data()), m_pos(0), m_size(buffer.size()), m_endian(endianness)
+        {}
+
+        /// Read data into a numeric value. Currently, this function only supports primitive numeric types.
+        /// Use other overloads for strings.
+        /// @tparam T primitive numberic type - please ensure this type's size is what you intend to read.
+        /// @param outVal [out] value to receive the data read
+        /// @returns number of bytes read; if > 0, `outVal` will contain the read data;
+        ///          if 0, `outVal` will be unmodified. Check sdgl::getError on 0 for more details.
+        template <typename T>
+        uint32_t read(T &outVal)
+        {
+            return readImpl(&outVal, sizeof(T));
+        }
+
+        template <typename T>
+        T getOr(T defaultVal = T())
+        {
+            auto result = read(defaultVal);
+            assert(result);
+
+            return defaultVal;
+        }
+
+        /// Read a null-terminated string from the buffer
+        /// @param outString [out] string to receive the data
+        /// @param maxSize         maximum number of characters to fill `outString`; the buffer position will still read
+        ///                        until the null-terminator, even if it exceeds this value, but such characters will
+        ///                        not be added to `outString`.
+        ///
+        /// @returns the number of bytes read (including null terminator) -
+        ///          if > 0 `outString` will contain data, otherwise `outString` will remain unmodified.
+        ///          Check sdgl::getError() for more information when 0 is returned.
+        ///          Note: this number may differ from string length + 1 if `maxSize` clipped the out value.
+        uint32_t read(std::string &outString, size_t maxSize = SIZE_MAX);
+
+        /// Read a string with an expected length
+        /// @param outString [out] string to receive the data
+        /// @param length          length of the string in bytes (not including null terminator)
+        /// @returns the number of bytes read (including null terminator) -
+        ///          if > 0 `outString` will contain data, otherwise `outString` will remain unmodified.
+        ///          Check sdgl::getError() for more information when 0 is returned.
+        uint32_t readFixedString(std::string &outString, size_t length);
+
+        /// Read a null-terminated string from the buffer
+        /// @param outBuffer [out] pointer to a string buffer to receive the data
+        /// @param maxSize         max length of `outBuffer`; the buffer position will still read until the
+        ///                        null-terminator, even if it exceeds this value, but such characters will not be added
+        ///                        to `outBuffer`.
+        ///
+        /// @returns the number of bytes read (including null terminator) -
+        ///          if > 0 `outBuffer` will contain data, otherwise `outBuffer` will remain unmodified.
+        ///          Check sdgl::getError for more info when 0 is returned.
+        ///          Note: this number may differ from string length + 1 if `maxSize` clipped the out value.
+        uint32_t read(char *outBuffer, size_t maxSize);
+
+
+        /// Peek relative to the current location. In debug mode, an assertion is made to check bounds.
+        /// @param offset - offset bytes, may be negative
+        ///
+        /// @returns value of byte at location
+        [[nodiscard]]
+        uint8_t peek(int offset = 0) const;
+
+        /// Move the view's position back to 0
+        void reset()
+        {
+            m_pos = 0;
+        }
+
+        /// Get the buffer size
+        [[nodiscard]]
+        auto size() const { return m_size; }
+
+        /// Get the number of bytes left to read (from current position to the end of the buffer)
+        [[nodiscard]]
+        auto bytesLeft() const { return m_size - m_pos; }
+
+        /// Get the view's byte position
+        [[nodiscard]]
+        auto position() const { return m_pos; }
+
+        /// Get the pointer to the data (at position 0)
+        [[nodiscard]]
+        const void *data() const { return m_buf; }
+
+        /// Get the expected endianness that was set in the view's constructor
+        [[nodiscard]]
+        auto expectedEndian() const { return m_endian; }
+
+        void move(size_t offset)
+        {
+            m_pos = offset;
+        }
+    protected:
+        uint32_t readImpl(void *buffer, size_t size);
+
+        uint8_t *m_buf;
+        size_t m_pos;
+        size_t m_size;
+        endian::type m_endian;
+    };
+
+    class BufferWriter : public BufferView
+    {
+    public:
+        BufferWriter(std::string &buffer, endian::type endianness) :
+            BufferView(buffer.data(), buffer.size(), endianness) { }
+
+        template <typename T>
+        uint32_t write(T value)
+        {
+            return writeImpl(&value, sizeof(T));
+        }
+        /// Write null-terminated string into the buffer
+        /// @note make sure there is enough space in the buffer for a null-terminator
+        /// @returns the number of bytes written, this includes the string's null terminator
+        uint32_t write(const std::string &str);
+
+        /// Write string into the buffer
+        /// @note make sure there is enough space for a null-terminator
+        /// @returns the number of bytes read (including null terminator)
+        uint32_t write(std::string_view str);
+
+        uint32_t write(const char *str, size_t length);
+    private:
+        uint32_t writeImpl(const void *data, size_t size);
+    };
+}
