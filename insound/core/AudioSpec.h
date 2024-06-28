c33418ae68b7429351f066f54485f20281d28984
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-06-28 07:10:40 -0700

    AudioSpec: add constructor overload to set all members

--- insound/core/AudioSpec.h
+++ insound/core/AudioSpec.h
@@ -4,7 +4,9 @@
 namespace insound {
     /// Description for data in a buffer or stream of audio
     struct AudioSpec {
-        AudioSpec(): freq(), channels(), format() { }
+        AudioSpec() : freq(), channels(), format() { }
+        AudioSpec(int freq, int channels, SampleFormat format) :
+            freq(freq), channels(channels), format(format) { }
 
         int freq;             ///< number of sample frames per second
         int channels;         ///< number of interleaved channels per frame
