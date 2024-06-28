1f6d68d91e96de6e3738865afe4a50067b6edac6
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-06-28 06:49:09 -0700

    logging.h: add Android logging functionality to remove dependency on `SDL_Log`

--- insound/core/logging.h
+++ insound/core/logging.h
@@ -1,9 +1,12 @@
+/// Logging macros for Insound Core
+/// Make sure not to put code with side-effects into this macro, as all statements will
+/// be stripped in Release mode when INSOUND_LOGGING is not defined.
 #pragma once
 
 #define VA_ARGS(...) , ##__VA_ARGS__
 
-#if INSOUND_DEBUG
-#ifdef __EMSCRIPTEN__
+#if defined(INSOUND_DEBUG) || defined(INSOUND_LOGGING)
+#ifdef __EMSCRIPTEN__      // HTML browser logging
 #include <cstdarg>
 #include <cstdio>
 #include <emscripten/webaudio.h>
@@ -29,17 +32,24 @@
 } while(0)
 
 
-#else // Non-emscripten logging
+#elif defined(__ANDROID__) // Android logging
+#include <android/log.h>
 
+#define INSOUND_LOG(format, ...) __android_log_print(ANDROID_LOG_INFO, "Insound", (format) VA_ARGS(__VA_ARGS__))
+#define INSOUND_ERR(format, ...) __android_log_print(ANDROID_LOG_ERROR, "Insound", (format) VA_ARGS(__VA_ARGS__))
+
+#else                      // Desktop, iOS logging
 #define INSOUND_LOG(format, ...) std::fprintf(stdout, (format) VA_ARGS(__VA_ARGS__))
 #define INSOUND_ERR(format, ...) std::fprintf(stderr, (format) VA_ARGS(__VA_ARGS__))
 
 #endif
 
 
-#else // Non debug: strip all logs
+#else // Non logging environments: strip all logs
 
-#define INSOUND_LOG(format, ...)
-#define INSOUND_ERR(format, ...)
+#include <insound/lib.h>
+
+#define INSOUND_LOG(format, ...) INSOUND_NOOP
+#define INSOUND_ERR(format, ...) INSOUND_NOOP
 
 #endif // INSOUND_DEBUG
